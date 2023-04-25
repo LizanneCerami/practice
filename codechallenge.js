2321ad0bac6b38b96284a0a8710f253cff1b76e5
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-04-24 21:20:16 -0400

    practicing loops

--- codechallenge.js
+++ codechallenge.js
@@ -1,34 +1,87 @@
-// // function person(fullName) {
-// //   const firstName = "liz"
-// //   const lastName = "cerami"
-// //   return fullName
+// // // // function person(fullName) {
+// // // //   const firstName = "liz"
+// // // //   const lastName = "cerami"
+// // // //   return fullName
+// // // // }
+
+// // // // console.log (${firstName} ${lastName})
+
+// // // // should have been:
+// // // function
+
+
+// // // for (let i = 0; i < 10;  i++) {
+// // //   console.log("I will never give up")
+// // // }
+
+
+// // // function Dog() {
+// // //   return (
+// // //     <main>
+// // //       <h1> My Dog Lucy</h1>
+// // //       <p>Lucy is my favorite puppy in the world.  I love her so much</p>
+// // //     </main>
+// // //   );
+// // // }
+
+// // function formHandler(firstName,lastName, email, phone){
+// // const newPerson = {
+// //   firstName: "liz",
+// //   lastName: "cerami",
+// //   email: "lizanne.cerami@gmail.com",
+// //   phone: "484-885-0156",
+// // }
 // // }
 
-// // console.log (${firstName} ${lastName})
+// function twoSum1(array, target) {
+//   for(let i = 0; i < array.length; i++) {
+//     for(let j = i + 1; j < array.length; j++) {
+//       if(array[i] + array[j] === target) {
+//         return [i,j]
+//       }
+//     }
+//   }
+// }
 
-// // should have been:
-// function
+// // the above solution 
 
+// // Challenge: solve Two Sum with a time complexity of On ("O of n").
 
-// for (let i = 0; i < 10;  i++) {
-//   console.log("I will never give up")
+// // We can interpret that as using only one loop.  How the heck can we do this?
+// // Our best bet is to add spatial complexity... in other words, store some data as we solve.
+
+// // We are going to colve again using something called a hash map (object)
+
+// function twoSum2(array,target) {
+//   let hashmap = {}
+//   for(let i = 0; i < array.length; i++) {
+//     const otherNumber = target - array[i]
+//     if(hashmap[otherNumber]) {       // does the other number already exist in hashmap?
+//       return [hashmap[otherNumber],i] // return that index, i
+//     } else {    // if not then let's put it in...
+//       hashmap[otherNumber] = i
+//     }
+//   }
 // }
 
 
-// function Dog() {
-//   return (
-//     <main>
-//       <h1> My Dog Lucy</h1>
-//       <p>Lucy is my favorite puppy in the world.  I love her so much</p>
-//     </main>
-//   );
+// for (let i = 0; i <= 100; i++) {
+
+//   if( i%3 === 0 )
+//   console.log("Fizz")
+
+//   else if( i%5 === 0 )
+//   console.log("Buzz")
+
+//   else (i%3 && i%5 === 0)
+//   console.log("FizzBuzz")
 // }
 
-function formHandler(firstName,lastName, email, phone){
-const newPerson = {
-  firstName: "liz",
-  lastName: "cerami",
-  email: "lizanne.cerami@gmail.com",
-  phone: "484-885-0156",
-}
+for (let i = 0; i <= 100; i++) {
+  if(i%3 ===  0)
+  console.log(`${i} Fizz`)
+  else if(i%5 === 0)
+  console.log(`${i} Buzz`)
+  else (i%3 && i%5 === 0)
+  console.log(`${i} FizzBuzz`)
 }
--- functions.js
+++ functions.js
@@ -1,11 +1,67 @@
-if ("15" === 15) {
-  console.log(true)
-} else {
-  console.log(false)
+// if ("15" === 15) {
+//   console.log(true)
+// } else {
+//   console.log(false)
+// }
+
+// function addNumbers(num1, num2){
+//   const total = num1 + num2
+//   return total
+// }
+
+// function isNumPalidrome(x) {
+//   const stringX = x = '';
+//   const midPoint = Math.floor(stringX.length / 2);
+//   for(let i = 0; i < midPoint; i++) {
+//     const start = i;
+//     const end = (stringX.length - 1) - i;
+//     if(stringX[start] !== stringX[end]) return false
+//   }
+//   return true
+// }
+
+// console.log(122);
+// console.log(1221)
+
+// given and array of pets where each element in the array has a pet.name, pet.age, and pet.type, add a new property to each element called woof and set to true if type: 'dog' or false for all others.
+
+// let pets = [
+//   { name: "Lucy", age: 6, type: "dog" },
+//   { name: "whiskers", age:12, type: "cat"}, 
+// ]
+
+// function addWoofToDogsUsingLoop (pets) {
+//   for( let i = 0; i <pets.length; i++) {  //looping through the array
+//     pets[i].woof = pets[i].type === 'dog'
+//   }
+//   return pets
+// }
+
+// addWoofToDogsUsingLoop(pets)
+// console.log(pets)
+
+// function addWoofToPetsUsingMap(pets) {
+//   pets = pets.mep (pet => {
+//   pet.woof = pet.type === 'dog'
+//   })
+//   return pets
+// }
+
+// given and array of pets where each element in the array has a pet.name, pet.age, and pet.type, add a new property to each element called woof and set to true if type: 'dog' or false for all others.
+
+let thesePets = [
+  {name: "Blue", age: 2, type: "dog"},
+  {name: "Jelly", age: 5, type: "cat"},
+  {name: "Tippy", age: 6, type: "dog"},
+  {name: "Skittles", age: 8, type: "lizard"},
+]
+
+function addWoofToDogs(thesePets) {
+  for(let i=0; i <= thesePets.length; i++) {
+  thesePets[i].woof = thesePets[i].type === "dog"
+}
+return thesePets
 }
 
-function addNumbers(num1, num2){
-  const total = num1 + num2
-  return total
-}
-
+addWoofToDogs(thesePets)
+console.log(thesePets)
